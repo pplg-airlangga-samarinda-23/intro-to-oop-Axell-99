b8e47b13a7f25bc9ecf803db8be47852c8ff910c
Yoga_Axel_Baim_Pratama <23_yogaxelbaim@student.smkti.net>
2024-10-02 21:27:44 +0800

    selesai pak

--- soal-3/Mobil.php
+++ soal-3/Mobil.php
@@ -1,15 +1,39 @@
 <?php
 class Mobil {
-    // lengkapi dengan atribut dan metode sesuai pada buku
+    public $merek;
+    public $plat;
+    public $model;
+    public $warna;
+    public $tahun;
+    public $kecepatan = 0;
 
-    // contoh metode melaju()
-    public function melaju() {
-        echo "Mobil sedang melaju.";
+    public function Melaju() {
+        echo "Mobil Sedang Melaju Kencang.";
     }
-    
+
+    public function Berhenti() {
+        $this->kecepatan = 0;
+        echo "Mobil Sedang Berhenti.";
+    }
+
+    public function gantiGigi($gigi) {
+        echo "Mobil mengganti gigi ke " . $gigi . ".";
+    }
+
+    public function keseluruhan() {
+        echo "Mobil: " . $this->merek . " Plat: " .$this ->plat . $this->model . " Warna: " . $this->warna . " Tahun: " . $this->tahun . " Sedang Melaju Dengan Kecepatan " . $this->kecepatan . " KM/Jam.";
+    }
 }
 
-// buat sebuah objek mobil dan lengkapi atributnya
+$mobilku = new Mobil();
+$mobilku->merek = "Nissan";
+$mobilku->plat = "XENZ GODD! ";
+$mobilku->model = "R34";
+$mobilku->warna = "Biru Putih";
+$mobilku->tahun = 1999;
 
-
-// panggil keempat metode yang terdapat pada kelas Mobil
+$mobilku->Melaju();
+$mobilku->gantiGigi(2);
+$mobilku->berhenti();
+$mobilku->keseluruhan();
+?>
